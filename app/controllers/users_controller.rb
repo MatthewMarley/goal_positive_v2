ce76e1442511c49ef2e09e8b3a7300f81970975d
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-08 07:33:59 +0000

    Added user validations, including valid_email_regex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.id
+            #session[:user_id] = @user.id
             flash[:success] = "Welcome to Goal Positive #{@user.username}"
-            redirect_to user_path(@user)
+            redirect_to root_path
         else
             render 'new'
         end
